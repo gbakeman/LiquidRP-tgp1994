9a7a7d09221de0fc9ff0e22dc99bce0475d3247a
Frankpetrov <jakekilsome@yahoo.com>
2014-08-03 12:45:32 -0400

    changed cl_paramedic
    blank but not empty

--- gamemode/modules/ldrp_health/cl_paramedic.lua
+++ gamemode/modules/ldrp_health/cl_paramedic.lua
@@ -1,15 +1,1 @@
-TEAM_PARAMEDIC = DarkRP.createJob("Paramedic", {
-	color = Color(57, 204, 204, 255),
-	model = "models/player/magnusson.mdl",
-	description = [[Run around looking for dumb people getting shot and help 
-	nurse them back to better health, or take them to the hospital if wounds
-	are too extreme]],
-	weapons = {"med_kit"},
-	command = "paramedic",
-	max = 2,
-	salary = 0,
-	admin = 0,
-	vote = false,
-	hasLicense = false,
-	medic = true
-})
+--yay its not empty
